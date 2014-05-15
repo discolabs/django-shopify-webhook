4e3db9dbd52d3a86380bdeeef0147ac1e29e7e4d
Gavin Ballard <gavin@gavinballard.com>
2014-05-15 18:54:16 +0200

    Update tests.

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.conf import settings
 from django.dispatch import receiver
 
-from .signals import order_created
+from .signals import orders_create
 from .helpers import get_hmac
 
 
@@ -33,25 +33,25 @@ class WebhookViewTestCase(TestCase):
         self.assertEqual(response.status_code, 400, 'Empty POST request returns 400 (Bad Request).')
 
     def test_no_hmac_is_forbidden(self):
-        response = self.post_shopify_webhook(topic = 'order/created', data = {'id': 123}, send_hmac = False)
-        self.assertEqual(response.status_code, 403, 'POST order/created request with no HMAC returns 403 (Forbidden).')
+        response = self.post_shopify_webhook(topic = 'orders/create', data = {'id': 123}, send_hmac = False)
+        self.assertEqual(response.status_code, 403, 'POST orders/create request with no HMAC returns 403 (Forbidden).')
 
     def test_invalid_hmac_is_forbidden(self):
-        response = self.post_shopify_webhook(topic = 'order/created', data = {'id': 123}, headers = {'HTTP_X_SHOPIFY_HMAC_SHA256': 'invalid'}, send_hmac = False)
-        self.assertEqual(response.status_code, 403, 'POST order/created request with invalid HMAC returns 403 (Forbidden).')
+        response = self.post_shopify_webhook(topic = 'orders/create', data = {'id': 123}, headers = {'HTTP_X_SHOPIFY_HMAC_SHA256': 'invalid'}, send_hmac = False)
+        self.assertEqual(response.status_code, 403, 'POST orders/create request with invalid HMAC returns 403 (Forbidden).')
 
     def test_valid_hmac_is_ok(self):
-        response = self.post_shopify_webhook(topic = 'order/created', data = {'id': 123})
-        self.assertEqual(response.status_code, 200, 'POST order/created request with valid HMAC returns 200 (OK).')
+        response = self.post_shopify_webhook(topic = 'orders/create', data = {'id': 123})
+        self.assertEqual(response.status_code, 200, 'POST orders/create request with valid HMAC returns 200 (OK).')
 
     def test_order_created_signal_triggered(self):
         data = {'id': 123456}
 
         # Create a test signal receiver for the order/created topic.
-        @receiver(order_created)
-        def test_order_created_receiver(sender, data, **kwargs):
-            test_order_created_receiver.data = data
-        test_order_created_receiver.data = None
+        @receiver(orders_create)
+        def test_order_create_receiver(sender, data, **kwargs):
+            test_order_create_receiver.data = data
+        test_order_create_receiver.data = None
 
-        response = self.post_shopify_webhook(topic = 'order/created', data = data)
-        self.assertEqual(data, test_order_created_receiver.data, 'POST order/created correctly triggered order_created signal.')
+        response = self.post_shopify_webhook(topic = 'orders/create', data = data)
+        self.assertEqual(data, test_order_create_receiver.data, 'POST orders/create correctly triggered order_created signal.')
