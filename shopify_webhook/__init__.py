ef67b4d319608c9e51fdee5ec79fb6bfdff1ecad
Gavin Ballard <gavin@gavinballard.com>
2014-09-10 16:07:05 +0200

    Add WEBHOOK_TOPICS list to module, bump version.

--- shopify_webhook/__init__.py
+++ shopify_webhook/__init__.py
@@ -1,3 +1,38 @@
-VERSION     = (0, 2, 0)
+VERSION     = (0, 2, 1)
 __version__ = '.'.join(map(str, VERSION))
 __author__ = 'Gavin Ballard'
+
+
+WEBHOOK_TOPICS = [
+    'orders/create',
+    'orders/delete',
+    'orders/updated',
+    'orders/paid',
+    'orders/cancelled',
+    'orders/fulfilled',
+    'orders/partially_fulfilled',
+    'carts/create',
+    'carts/update',
+    'checkouts/create',
+    'checkouts/update',
+    'checkouts/delete',
+    'refunds/create',
+    'products/create',
+    'products/update',
+    'products/delete',
+    'collections/create',
+    'collections/update',
+    'collections/delete',
+    'customer_groups/create',
+    'customer_groups/update',
+    'customer_groups/delete',
+    'customers/create',
+    'customers/enable',
+    'customers/disable',
+    'customers/update',
+    'customers/delete',
+    'fulfillments/create',
+    'fulfillments/update',
+    'shop/update',
+    'app/uninstalled',
+]
