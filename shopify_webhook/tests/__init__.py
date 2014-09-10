7d81095f148cab41d2987236563b3584e6307b95
Gavin Ballard <gavin@gavinballard.com>
2014-09-10 22:39:54 +0200

    Tweak fixture loading.

--- shopify_webhook/tests/__init__.py
+++ shopify_webhook/tests/__init__.py
@@ -15,15 +15,13 @@ class AbstractWebhookTestCase(TestCase):
         self.webhook_url = reverse('webhook')
 
     def post_shopify_webhook(self, topic = None, domain = None, data = None, headers = None, send_hmac = True):
-        # Ensure data is a JSON-encoded string.
-        if isinstance(data, dict):
-            data = json.dumps(data)
-        elif data is None or not isinstance(data, basestring):
-            data = '{}'
-
         # Set defaults.
+        domain = 'test.myshopify.com' if domain is None else domain
+        data = {} if data is None else data
         headers = {} if headers is None else headers
-        domain = domain if domain is not None else 'test.myshopify.com'
+
+        # Dump data as a JSON string.
+        data = json.dumps(data)
 
         # Add required headers.
         headers['HTTP_X_SHOPIFY_TEST'] = 'true'
@@ -37,7 +35,7 @@ class AbstractWebhookTestCase(TestCase):
 
         return self.client.post(self.webhook_url, data = data, content_type = 'application/json', **headers)
 
-    def load_fixture(self, name, format = 'json'):
-        fixture_path = "{0}/fixtures/{1}.{2}".format(os.path.dirname(sys.modules[self.__module__].__file__), name, format)
+    def read_fixture(self, name):
+        fixture_path = "{0}/fixtures/{1}.json".format(os.path.dirname(sys.modules[self.__module__].__file__), name, format)
         with open(fixture_path, 'rb') as f:
-            return f.read()
+            return json.loads(f.read())
