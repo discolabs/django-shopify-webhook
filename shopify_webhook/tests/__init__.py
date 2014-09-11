cef10e465f7aa36c37031306e6e79d9f246d7b8f
Gavin Ballard <gavin@gavinballard.com>
2014-09-11 11:28:21 +0200

    Add comments to web hook test case.

--- shopify_webhook/tests/__init__.py
+++ shopify_webhook/tests/__init__.py
@@ -8,13 +8,23 @@ from django.conf import settings
 from ..helpers import get_hmac
 
 
-class AbstractWebhookTestCase(TestCase):
+class WebhookTestCase(TestCase):
+    """
+    A base class for running tests on Shopify webhooks. Can be used by `shopify_webhook` tests here, or by other
+    packages that utilise webhook behaviour.
+    """
 
     def setUp(self):
-        super(AbstractWebhookTestCase, self).setUp()
+        """
+        Set up the test case, primarily by getting a reference to the webhook endpoint to be used for testing.
+        """
+        super(WebhookTestCase, self).setUp()
         self.webhook_url = reverse('webhook')
 
     def post_shopify_webhook(self, topic = None, domain = None, data = None, headers = None, send_hmac = True):
+        """
+        Simulate a webhook being sent to the application's webhook endpoint with the provided parameters.
+        """
         # Set defaults.
         domain = 'test.myshopify.com' if domain is None else domain
         data = {} if data is None else data
@@ -36,6 +46,11 @@ class AbstractWebhookTestCase(TestCase):
         return self.client.post(self.webhook_url, data = data, content_type = 'application/json', **headers)
 
     def read_fixture(self, name):
+        """
+        Read a .json fixture with the specified name, parse it as JSON and return.
+        Currently makes the assumption that a directory named 'fixtures' containing .json files exists and is located
+        in the same directory as the file running the tests.
+        """
         fixture_path = "{0}/fixtures/{1}.json".format(os.path.dirname(sys.modules[self.__module__].__file__), name, format)
         with open(fixture_path, 'rb') as f:
             return json.loads(f.read())
