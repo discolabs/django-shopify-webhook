af1d4a5ce1e5e78960798e1f68bec6f49f47f860
Andres Douglas <andres.douglas@gmail.com>
2016-05-24 20:34:54 -0700

    switching from robust to normal signals for debugging

--- shopify_webhook/views.py
+++ shopify_webhook/views.py
@@ -32,8 +32,8 @@ class WebhookView(View):
         # Convert the topic to a signal name and trigger it.
         signal_name = get_signal_name_for_topic(request.webhook_topic)
         try:
-            signals.webhook_received.send_robust(self, domain = request.webhook_domain, topic = request.webhook_topic, data = request.webhook_data)
-            getattr(signals, signal_name).send_robust(self, domain = request.webhook_domain, topic = request.webhook_topic, data = request.webhook_data)
+            signals.webhook_received.send(self, domain = request.webhook_domain, topic = request.webhook_topic, data = request.webhook_data)
+            getattr(signals, signal_name).send(self, domain = request.webhook_domain, topic = request.webhook_topic, data = request.webhook_data)
         except AttributeError:
             return HttpResponseBadRequest()
 
